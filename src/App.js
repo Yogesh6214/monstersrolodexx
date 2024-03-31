5ffe73d4bc1d155af48ec9c19fce186550f9f0a5
Yogesh Kumar <globalaudios736@gmail.com>
2024-03-31 14:12:19 +0530

    Modifying the App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Monsters Rolodex
-        </p>
-        <a
-          className="App-link"
-          href="https://monstaersrolodex.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Monsters Rolodex App
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      String: "Hi, Yogesh",
+    };
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Monsters Rolodex</p>
+          <a
+            className="App-link"
+            href="https://monstaersrolodex.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Monsters Rolodex App
+          </a>
+          <p>{this.state.String}</p>
+          <button
+            style={{
+              backgroundColor: "#61dafb",
+              border: "none",
+              color: "white",
+              padding: "5px 15px",
+              textAlign: "center",
+              textDecoration: "none",
+              display: "inline-block",
+              fontSize: "16px",
+              margin: "4px 2px",
+              cursor: "pointer",
+            }}
+            onClick={() =>
+              this.setState({ String: "Welcome to Monsters Rolodex App" })
+            }
+          >
+            Click Me
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
